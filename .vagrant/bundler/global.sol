d0d66a0e2591f3011e50ea44dbd6991e2ad6f3c7
oh.willy@hotmail.com <oh.willy@hotmail.com>
2022-03-19 22:22:45 -0400

    vagrant file for ansible

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-berkshelf",["= 5.1.2"]],["vagrant-hostmanager",["= 1.8.9"]]],"checksum":"ab0d89defe6eae812ff37067afd72bf05252278e1f855705d8028846f5c81e02","vagrant_version":"2.2.19"}
